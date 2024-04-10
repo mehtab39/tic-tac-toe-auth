a0eb1f7f328bca645c284303af1f812e15226ae6
mehtab39 <mehtabgill122@gmail.com>
2024-04-10 21:53:11 +0530

    fix: ping handler

--- main.go
+++ main.go
@@ -33,9 +33,19 @@ func main() {
 	http.HandleFunc("/api/signin", handlers.SigninHandler)
 	http.HandleFunc("/api/game-stats", handlers.StatsHandler)
 	http.HandleFunc("/api/game-stats/", handlers.StatsHandler)
+	http.HandleFunc("/ping", pingHandler)
 
 	http.HandleFunc("/api/combine/user/", handlers.GetCombineUserInfo)
 
 	log.Printf("Listening to port %d", 8081)
 	http.ListenAndServe(":8081", middlewares.CORSPolicy(http.DefaultServeMux))
+}
+
+func pingHandler(w http.ResponseWriter, r *http.Request) {
+	_, err := w.Write([]byte("pong"))
+	if err != nil {
+		// Handle error if writing to response fails
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
 }
